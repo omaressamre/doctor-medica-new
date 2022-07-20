f9ae20f0ff96866dcf5be32ac7064548b246fea0
Omar Essam <essam.omar79@yahoo.com>
2022-07-20 04:44:05 +0200

    update colors

--- lib/doctor/doctor_profile_navpage.dart
+++ lib/doctor/doctor_profile_navpage.dart
@@ -153,7 +153,7 @@ class doctor_profilenav extends StatelessWidget {
                             child: Row(
                               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                SvgPicture.asset('assets/images/User.svg'),
+                                SvgPicture.asset('assets/images/User.svg',color: secondaryColor,),
                                 SizedBox(width: size.width * 0.04),
                                 CustomText(
                                   text: 'Profile',
@@ -243,7 +243,7 @@ class doctor_profilenav extends StatelessWidget {
                               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 SvgPicture.asset(
-                                    'assets/images/information-svgrepo-com.svg'),
+                                    'assets/images/information-svgrepo-com.svg',color: secondaryColor,),
                                 SizedBox(width: size.width * 0.04),
                                 CustomText(
                                   text: 'About us',
